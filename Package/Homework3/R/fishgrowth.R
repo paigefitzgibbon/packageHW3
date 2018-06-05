54988ddef1cd58e6114e46e1f09b2640f5d38e13
Paige FitzGibbon <paigepeppers15@yahoo.com>
2018-06-04 19:38:33 -0700

    homework second commit

--- Package/Homework3/R/fishgrowth.R
+++ Package/Homework3/R/fishgrowth.R
@@ -1,12 +1,11 @@
 #' Predicts fish growth rate based on temperature
+#' @author Celine Mol and Paige FitzGibbon
+#' @examples fishgrowth(0.1,0.2,0.3,0.4, T=seq(from = 0, to = 18, by = 2))
 #' @param T temperature of fishery in degrees celsius
-#' @param coefficients
-#' \describe{
-#' \item a {zero order parameter}
-#' \item b {first order parameter}
-#' \item c {second order parameter}
-#' \item d{third order parameter}
-#' }
+#' @param a zero-order coefficient
+#' @param b first-order coefficient
+#' @param c second-order coefficient
+#' @param d third-order coefficient
 #' @references https://www.sciencedirect.com/science/article/abs/pii/S0044848607005182
 
 fishgrowth <- function(a,b,c,d,T) {
